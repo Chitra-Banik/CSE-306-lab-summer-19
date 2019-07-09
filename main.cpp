17121f5efc35534d6537e1941479c78c60020451
Chitra-Banik <51938187+Chitra-Banik@users.noreply.github.com>
2019-07-09 10:59:39 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,99 +1,57 @@
 #include<iostream>
+#include<fstream>
 #include<cstdlib>
 #include<ctime>
-#include<fstream>
 using namespace std;
-void Merge(int A[],int p, int q,int r)
+void bubblesort(int arr[],int n)
 {
-
-	int n1=q-p+1,i,j,k;
-
-	int n2=r-q;
-
-	int left[n1],right[n2];
-
-	for(i=0;i<n1;i++)
-
-	{
-		left[i]=A[p+i];
-	}
-
-	for(j=0;j<n2;j++)
-	{
-		right[j]=A[q+j+1];
-	}
-
-	i=0,j=0;
-
-	for(k=p;i<n1&&j<n2;k++)
-	{
-		if(left[i]<right[j])
-		{
-			A[k]=left[i++];
-		}
-		else
-		{
-			A[k]=right[j++];
-		}
-	}
-
-	while(i<n1)
-	{
-		A[k++]=left[i++];
-	}
-
-	while(j<n2)
-	{
-		A[k++]=right[j++];
-	}
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
 }
-
-void MergeSort(int A[],int p,int r)
-{
-	int q;
-
-	if(p<r)
-	{
-		q=(p+r)/2;
-		MergeSort(A,p,q);
-		MergeSort(A,q+1,r);
-		Merge(A,p,q,r);
-	}
-
-}
-clock_t startTime=clock();
 int main()
 {
 
 
     ofstream fileinput;
-    srand(time(NULL));
-    fileinput.open("100k.txt");
+    srand(89345);
+    fileinput.open("10.txt");
     int n=100000;
-
-   int A[n];
+    //fileinput>>n;
+    int*arr=new int[n];
 
     for(int i=0;i<n;i++)
     {
-         A[i]=rand()%5000;
         fileinput<<rand()<<endl;
-
+        arr[i]=rand();
     }
-    fileinput.close();
 
-      MergeSort(A,0,n-1);
+    fileinput.close();
+    //atrat time
+    clock_t startTime=clock();
+    bubblesort(arr,n);
+    //end time
+    clock_t endTime=clock();
 
     ofstream fileoutput;
-    fileoutput.open("100koutput.txt");
- clock_t endTime=clock();
+    fileoutput.open("output10.txt");
     for(int i=0;i<100000;i++)
     {
-
-        fileoutput<<A[i]<<endl;
+        fileoutput<<arr[i]<<endl;
     }
+    //count the lapse
     double timelapse=double (endTime-startTime)/CLOCKS_PER_SEC;
-    cout<<timelapse<<endl;
-
+    cout<<"timelapse: "<<timelapse<<endl;
+    fileoutput<<"timelapse: "<<timelapse<<endl;
     fileoutput.close();
-    delete[] A;
+    delete[] arr;
 }
