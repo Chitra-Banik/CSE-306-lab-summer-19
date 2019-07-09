6613fe17a7a56ea9dc9fd1771da069aba03009fb
Chitra-Banik <51938187+Chitra-Banik@users.noreply.github.com>
2019-07-09 10:58:55 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,58 +1,99 @@
 #include<iostream>
- #include <fstream>
+#include<cstdlib>
+#include<ctime>
+#include<fstream>
 using namespace std;
+void Merge(int A[],int p, int q,int r)
+{
 
- void insert(int arr[],int n)
- {
+	int n1=q-p+1,i,j,k;
 
-    for(int i=1;i<=n-1;i++)
-    {
-        int temp=arr[i];
-        int j=i-1;
+	int n2=r-q;
 
-        while((temp<arr[j])&&(j>=0))
-        {
-            arr[j+1]=arr[j];    //moves element forward
-            j=j-1;
-        }
+	int left[n1],right[n2];
 
-        arr[j+1]=temp;    //insert element in proper place
-    }
- }
+	for(i=0;i<n1;i++)
+
+	{
+		left[i]=A[p+i];
+	}
+
+	for(j=0;j<n2;j++)
+	{
+		right[j]=A[q+j+1];
+	}
+
+	i=0,j=0;
+
+	for(k=p;i<n1&&j<n2;k++)
+	{
+		if(left[i]<right[j])
+		{
+			A[k]=left[i++];
+		}
+		else
+		{
+			A[k]=right[j++];
+		}
+	}
+
+	while(i<n1)
+	{
+		A[k++]=left[i++];
+	}
+
+	while(j<n2)
+	{
+		A[k++]=right[j++];
+	}
+}
+
+void MergeSort(int A[],int p,int r)
+{
+	int q;
+
+	if(p<r)
+	{
+		q=(p+r)/2;
+		MergeSort(A,p,q);
+		MergeSort(A,q+1,r);
+		Merge(A,p,q,r);
+	}
+
+}
+clock_t startTime=clock();
 int main()
 {
-    int i;
 
 
-    ifstream FileInput;// read from file
-    FileInput.open("input.txt");
-    int n;
-    FileInput >> n;
-    int *arr=new int[n];
-    for(int i=0; i< n; i++)
+    ofstream fileinput;
+    srand(time(NULL));
+    fileinput.open("100k.txt");
+    int n=100000;
+
+   int A[n];
+
+    for(int i=0;i<n;i++)
     {
-        FileInput >> arr[i];
-    }
-    // after completing reading
-    FileInput.close();
-
-
-    //sorting
-    //  bubbleSort(arr, n);
-    insert(arr,n);
-
-    ofstream FileOutput;
-
-    FileOutput.open("output.txt");
-
-    for(int i=0; i<n; i++)
-    {
-        FileOutput << arr[i]<< " ";
+         A[i]=rand()%5000;
+        fileinput<<rand()<<endl;
 
     }
-    FileOutput.close();
+    fileinput.close();
 
-    delete[] arr;
+      MergeSort(A,0,n-1);
 
-    return 0;
+    ofstream fileoutput;
+    fileoutput.open("100koutput.txt");
+ clock_t endTime=clock();
+    for(int i=0;i<100000;i++)
+    {
+
+        fileoutput<<A[i]<<endl;
+    }
+    double timelapse=double (endTime-startTime)/CLOCKS_PER_SEC;
+    cout<<timelapse<<endl;
+
+    fileoutput.close();
+    delete[] A;
 }
